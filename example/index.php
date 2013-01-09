0f7f6a6c071c80968e091d6e7af73b3f113220be
Anthony Mills <anthony.mills@sapientnitro.com>
2013-01-09 17:11:21 +1100

    Submission of URL via AJAX to the class

--- example/index.php
+++ example/index.php
@@ -1,6 +1,18 @@
 <?php
 if(isset($_POST['page_url'])) {
+	$apiKey = 'Enter Your API Key';
+	require_once('../includes/GooglePageSpeed.php');
 	
+	$pageSpeed = new GooglePageSpeed($apiKey);
+	$pageResult = $pageSpeed->checkUrl($_POST['page_url']);
+	
+	if ((!empty($pageResult->responseCode) && ($pageResult->responseCode == '200'))) {
+		$pageReport = file_get_gontents('views/page_report.phtml');
+		
+	} else {
+		echo '<span class="error_msg">The URL provided was unable to be retrieved.</span>';
+	}
 } else {
-	echo file_get_contents('views/check_url.phtml');
+	$pageContents = file_get_contents('views/check_url.phtml');
+	echo str_replace('{script_url}', $_SERVER['PHP_SELF'], $pageContents);
 } 
--- includes/GooglePageSpeed.php
+++ includes/GooglePageSpeed.php
@@ -10,12 +10,13 @@
 */
 class GooglePageSpeed {
 	protected $_apiKey = '';
-	protected $_prettyPrint = FALSE;
+	protected $_prettyPrint = TRUE;
+	protected $_apiEndpoint = 'https://www.googleapis.com/pagespeedonline/v1/runPagespeed';
 	
-	public function __construct(str $apiKey = NULL) 
+	public function __construct($apiKey = NULL) 
 	{
 		if (empty($apiKey)) {
-			throw new Exception('No Google API key provided');
+			die('<span class="error_msg">No Google API key provided.</span>');
 		} else {
 			$this->_apiKey = $apiKey;
 		}
@@ -44,36 +45,43 @@ class GooglePageSpeed {
 	 * @param string $pageUrl
 	 *  
 	 */
-	public function checkUrl(str $pageUrl)
+	public function checkUrl($pageUrl)
 	{
 		if (filter_var($pageUrl, FILTER_VALIDATE_URL) === FALSE) {
- 		   throw new Exception('Url provided is not valid');
+ 		   die('<span class="error_msg">The URL provided is not valid.</span>');
 		} else {
 			$result = $this->_processRequest($pageUrl);
+			print_r($result);
 		}
 	}
 	
 	/**
 	 * Contact the PageSpeed API and request the feedback on a URL
 	 * 
-	 * @param string $url
-	 * @return array
+	 * @param string $pageUrl
+	 * @return object
 	 */
-	protected function _processRequest(str $apiRequest)
+	protected function _processRequest($pageUrl)
 	{
-		$curlHandle = curl_init($apiRequest);	
-		        curl_setopt($curlHandle, CURLOPT_VERBOSE, 1);
-		        curl_setopt($curlHandle, CURLOPT_NOBODY, 0);
-		        curl_setopt($curlHandle, CURLOPT_HEADER, 0);
-		        curl_setopt($curlHandle, CURLOPT_FOLLOWLOCATION,1);
-		        curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
+		$apiRequest = $this->_apiEndpoint . '?key=' . $this->_apiKey . '&url=' . $pageUrl;
+		
+		if (!$this->_prettyPrint) {
+			$apiRequest .= '&prettyprint=false';
+		}
+		
+		$curlHandle = curl_init($apiRequest);
+		curl_setopt($curlHandle, CURLOPT_VERBOSE, 1);
+		curl_setopt($curlHandle, CURLOPT_NOBODY, 0);
+		curl_setopt($curlHandle, CURLOPT_HEADER, 0);
+		curl_setopt($curlHandle, CURLOPT_FOLLOWLOCATION,1);
+		curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
 		
 		$apiResponse = curl_exec($curlHandle);
 		$responseInformation = curl_getinfo($curlHandle);
-		curl_close($ch);
+		curl_close($curlHandle);
 		
 		if( intval( $responseInformation['http_code'] ) == 200 ) {
-			return json_decode($apiResponse);	
+			return json_decode($apiResponse);
 		}	
 	}	
 }
