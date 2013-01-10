0cad85a4e404f92bd48af3eedd59fa073551da40
Anthony Mills <anthony.mills@sapientnitro.com>
2013-01-10 15:22:52 +1100

    Papespeed report via AJAX

--- example/index.php
+++ example/index.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['page_url'])) {
-	$apiKey = 'Enter Your API Key';
+	$apiKey = 'MY API KEY';
 	require_once('../includes/GooglePageSpeed.php');
 	
 	$pageSpeed = new GooglePageSpeed($apiKey);
@@ -8,6 +8,8 @@ if(isset($_POST['page_url'])) {
 	
 	if ((!empty($pageResult->responseCode) && ($pageResult->responseCode == '200'))) {
 		$pageReport = file_get_gontents('views/page_report.phtml');
+		
+		$pageReport = str_replace('{page_score}', $pageResult->score, $subject);
 		
 	} else {
 		echo '<span class="error_msg">The URL provided was unable to be retrieved.</span>';
